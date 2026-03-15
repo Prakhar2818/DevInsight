289c6622225f2999368d8dd5880d7ec5f62a28d6
Prakhar2818 <prakharattarde95@gmail.com>
2026-03-15 10:30:51 +0530

    backend deploy-5

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './src/app.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-      origin: [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-      ],
+      origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       credentials: true,
     });
