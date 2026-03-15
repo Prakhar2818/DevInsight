e2e50c01dc0ecb7da2de3a6edd69116e56352e33
Prakhar2818 <prakharattarde95@gmail.com>
2026-03-15 11:19:00 +0530

    frontend deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     app.enableCors({
       origin: [
         'http://localhost:3000',
-        'https://7385a7c2.devinsight-bm7.pages.dev/dashboard',
+        'https://801d166b.devinsight-bm7.pages.dev/',
 
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
