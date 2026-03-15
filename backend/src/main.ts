83b3d53a38ea9dbe93facf6d420ede016a53014f
Prakhar2818 <prakharattarde95@gmail.com>
2026-03-15 10:55:23 +0530

    backend deploy-6

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,13 +6,17 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-      origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+      origin: [
+        'http://localhost:3000',
+        'https://7385a7c2.devinsight-bm7.pages.dev/dashboard',
+
+      ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       credentials: true,
     });
 
     const port = Number(process.env.PORT || 4000);
-    await app.listen(port);
+    await app.listen(port, '0.0.0.0');
 
     console.log(`Server started on port ${port}`);
   } catch (error) {
