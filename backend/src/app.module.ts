a9fde80fd8460a1ee62b0032cdd9dae9b24e6905
Prakhar2818 <prakharattarde95@gmail.com>
2026-03-15 11:03:18 +0530

    backend deploy-7

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,7 +6,7 @@ import { RepoModule } from './repo/repo.module';
 import { ParserModule } from './parser/parser.module';
 import { DebugModule } from './debug/debug.module';
 import { FileModule } from './file/file.module';
-import { DocsModule } from '../src/docs/docs.module';
+import { DocsModule } from './docs/docs.module';
 import { DiagramModule } from '../src/diagram/diagram.module';
 import { DatabaseModule } from '../src/database/database.module';
 import { ConfigModule } from '@nestjs/config';
