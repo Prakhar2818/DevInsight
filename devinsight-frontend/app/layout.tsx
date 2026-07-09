7b1025147f4f2149feb9a834864a4b458ed1ba38
Prakhar2818 <prakharattarde95@gmail.com>
2026-07-09 12:46:26 +0530

    feat: initialize project structures and core modules for DevInsight, Galecto, and YatraSetu repositories

--- devinsight-frontend/app/layout.tsx
+++ devinsight-frontend/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.scss";
+import "./globals.css";
 import { Inter, Oswald } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import AnimatedBackground from "@/components/AnimatedBackground";
